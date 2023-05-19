19bb44135d1e1f8554c0934917f540234ee88dc6
santowiliamhabibu <127588647+santowiliamhabibu@users.noreply.github.com>
2023-05-19 22:11:42 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+   var typed = new Typed('#elemen', {
+      strings: ['hoshi bakery','japanese healt bread'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop:true
+    });
+    var typed = new Typed('#spicy', {
+        strings: ['japanese bread'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop:true
+      });
